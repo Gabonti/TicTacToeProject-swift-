23f320f02704ab42db47c14adeae10d39180fc16
gabonti <gabdilkarim05@mail.ru>
2024-04-14 13:20:45 +0500

    Initial Commit

---
+++ TicTacToeProject/TicTacToeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeProjectApp.swift
+//  TicTacToeProject
+//
+//  Created by Gabdilkarim Ata on 14.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
