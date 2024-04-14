dff7909f1b88464620795dc334e90c6c4a5138ce
gabonti <gabdilkarim05@mail.ru>
2024-04-14 13:22:31 +0500

    main menu

---
+++ TicTacToeProject/EnterNameView.swift
@@ -0,0 +1,18 @@
+//
+//  EnterNameView.swift
+//  TicTacToeProject
+//
+//  Created by Gabdilkarim Ata on 14.04.2024.
+//
+
+import SwiftUI
+
+struct EnterNameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnterNameView()
+}
